cb84d19a0758e46890c98518aff9fc24af3bfc84
Pihl17 <Mpihl17@student.aau.dk>
2018-04-09 09:49:04 +0200

    Fixed issues

--- P2 Prototype/Assets/_Scripts/Movement.cs
+++ P2 Prototype/Assets/_Scripts/Movement.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:P2 Prototype/Assets/_Scripts/Movement.cs
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,31 +13,7 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-
-        transform.Translate(moveHorizontal*speed, 0f, moveVertical*speed);
-	}
-
-}
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Movement : MonoBehaviour {
-
-    public float speed;
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        move(transform);
+		move(transform);
 	}
     public void move(Transform trans)
     {
@@ -50,4 +25,3 @@ public class Movement : MonoBehaviour {
         
     }
 }
->>>>>>> Zmove:P2 Prototype/Assets/Movement.cs
