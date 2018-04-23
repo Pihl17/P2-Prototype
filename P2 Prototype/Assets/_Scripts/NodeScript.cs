863752bccb9bb3f7c51d7b0f70a1ea9243bed647
MPihl <mpihl17@student.aau.dk>
2018-04-23 14:45:02 +0200

    Changes in the Node tracking script

--- P2 Prototype/Assets/_Scripts/NodeScript.cs
+++ P2 Prototype/Assets/_Scripts/NodeScript.cs
@@ -5,37 +5,33 @@ using UnityEngine.XR;
 
 public class NodeScript : MonoBehaviour {
 
-	public enum nodeType {leftHand, rightHand};
-	public nodeType typeOfNode;
+	public XRNode nodeType;
 
+	Vector3 offSet;
 	public Vector3 deltaPos;
 
 	// Use this for initialization
 	void Start () {
-		TrackPosition ();
+		offSet = transform.localPosition;
+		TrackPosition(nodeType);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 oldPos = transform.position;
-		TrackPosition ();
-		deltaPos = transform.position - oldPos;
-	}
-
-	void TrackPosition () {
-		if (typeOfNode == nodeType.leftHand)
-			TrackPosition (XRNode.LeftHand);
-		else if (typeOfNode == nodeType.rightHand)
-			TrackPosition (XRNode.RightHand);
+		Vector3 oldPos = transform.localPosition;
+		TrackPosition(nodeType);
+		deltaPos = transform.localPosition - oldPos;
+		if (Input.GetButton("Fire2") && nodeType == XRNode.RightHand || Input.GetButton("Jump") && nodeType == XRNode.LeftHand)
+			offSet += InputTracking.GetLocalPosition(nodeType);
 	}
 
 	void TrackPosition (XRNode node) {
-		transform.localPosition = InputTracking.GetLocalPosition(node);
+		transform.localPosition = InputTracking.GetLocalPosition(node) + offSet;
 		transform.rotation = InputTracking.GetLocalRotation(node);
 	}
 
 	public Vector3 getPosition() {
-		return transform.position;
+		return transform.position; 
 	}
 
 	static public bool Above(NodeScript node1, NodeScript node2) {
