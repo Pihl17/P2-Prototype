b8795852e21949ce0fe7637dcdc4628ca85bf5f6
MPihl <mpihl17@student.aau.dk>
2018-04-24 15:05:07 +0200

    Cleaned up some code and made the movement to only forward

--- P2 Prototype/Assets/_Scripts/Movement.cs
+++ P2 Prototype/Assets/_Scripts/Movement.cs
@@ -4,87 +4,30 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	NodeScript[] nodes;
-	//float angleMargin = 45;
+	public NodeScript[] nodes;
+	public float angleMargin = 45;
 
-    public float speed;
-
-	// Use this for initialization
-	void Start () {
-		GameObject[] trackingObjects = GameObject.FindGameObjectsWithTag ("Trackers");
-		nodes = new NodeScript[trackingObjects.GetLength(0)];
-
-		for (int i = 0; i < trackingObjects.GetLength (0) && i < nodes.GetLength (0); i++) {
-			if (trackingObjects [i].GetComponent<NodeScript> () != null)
-				nodes [i] = trackingObjects [i].GetComponent<NodeScript> ();
-			else
-				Debug.LogErrorFormat (trackingObjects [i], "No NodeScript on Object!"); 
-				// Logs an error message, if one then clicked on the message would the object put in as the first argument be highlighted;
-				// In this case showing what object is missing the script or is wrongly tagged as a tracker.
-		}
-
-		// Just for some debugging
-		if (nodes.GetLength (0) < 2)
-			Debug.LogErrorFormat ("There seems to be less tracking objects than expected...");
-		else if (nodes.GetLength (0) > 2)
-			Debug.LogErrorFormat ("There seems to be more tracking objects than expected...");
-		
-	}
+    public float speed = 5;
 	
 	// Update is called once per frame
 	void Update	() {
-		if (nodes.GetLength (0) >= 2) {
-			float totalMagnitude = nodes[0].deltaPos.magnitude + nodes[1].deltaPos.magnitude;
-
-			/*if (NodeScript.Above (nodes [0], nodes [1]) && CheckRequiredMovement(nodes[0].deltaPos))
-				Move (nodes[0].deltaPos, totalMagnitude);
-			else if (NodeScript.Above (nodes [1], nodes [0]) && CheckRequiredMovement(nodes[1].deltaPos))
-				Move (nodes[1].deltaPos, totalMagnitude);*/
-
-			if (CheckRequiredMovement (nodes [0].deltaPos) && CheckRequiredMovement (nodes [1].deltaPos)) {
-				foreach (NodeScript node in nodes) {
-					if (node.Above (nodes [0]) || node.Above (nodes [1]))
-						Move (node.deltaPos, totalMagnitude);
-				}
+		Move(Time.deltaTime);
+		foreach (NodeScript node in nodes) {
+			if (CheckRequiredMovement(node.deltaPos)) {
+				Move(node.deltaPos.magnitude);
 			}
 		}
-
 	}
 
-	void Move(Vector3 direction, float distance) {
-		if (direction.y != 0) direction.y = 0;
-		float angle = Vector3.Angle(Vector3.forward, direction);
-
-		transform.Translate ( new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * distance * speed);
+	void Move(float distance) {
+		transform.Translate(distance*speed*Vector3.forward);
 	}
-
-
-
-    void Move(Transform trans) {
-		Move(trans, Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-    }
-
-	void Move(Transform trans, float moveHorizontal, float moveVertical) {
-		float angle = trans.rotation.eulerAngles.y * Mathf.Deg2Rad;
-
-		transform.Translate(-moveHorizontal * speed * Mathf.Cos(angle) + moveVertical * speed * Mathf.Sin(angle), 
-							0f, 
-							moveHorizontal * speed * Mathf.Sin(angle) + moveVertical * speed * Mathf.Cos(angle));
-		
-	}
-
-
 
 	bool CheckRequiredMovement(Vector3 direction) {
-		return true;
-		/*
 		if (direction == Vector3.zero) return false; 
-		float angle = Vector3.Angle(Vector3.right + Vector3.forward, direction);
-		if (angle <= angleMargin || angle >= 180-angleMargin) {
-			return true;
-		} else
-			return false;
-		*/
+		float angle = Vector3.Angle(Vector3.forward, direction);
+		return (angle <= angleMargin || angle >= 180-angleMargin);
+
 	}
 
 }
--- P2 Prototype/Assets/_Scripts/NodeScript.cs
+++ P2 Prototype/Assets/_Scripts/NodeScript.cs
@@ -5,45 +5,23 @@ using UnityEngine.XR;
 
 public class NodeScript : MonoBehaviour {
 
-	public enum nodeType {leftHand, rightHand};
-	public nodeType typeOfNode;
-
+	public XRNode nodeType;
 	public Vector3 deltaPos;
+	Vector3 oldPos;
 
 	// Use this for initialization
 	void Start () {
-		TrackPosition ();
+		oldPos = TrackPosition(nodeType);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 oldPos = transform.position;
-		TrackPosition ();
-		deltaPos = transform.position - oldPos;
+		deltaPos = TrackPosition(nodeType) - oldPos;
+		oldPos = TrackPosition(nodeType);
 	}
 
-	void TrackPosition () {
-		if (typeOfNode == nodeType.leftHand)
-			TrackPosition (XRNode.LeftHand);
-		else if (typeOfNode == nodeType.rightHand)
-			TrackPosition (XRNode.RightHand);
-	}
-
-	void TrackPosition (XRNode node) {
-		transform.localPosition = InputTracking.GetLocalPosition(node);
-		transform.rotation = InputTracking.GetLocalRotation(node);
-	}
-
-	public Vector3 getPosition() {
-		return transform.position;
-	}
-
-	static public bool Above(NodeScript node1, NodeScript node2) {
-		return node1.getPosition().y > node2.getPosition().y;
-	}
-
-	public bool Above(NodeScript node) {
-		return Above(this, node);
+	Vector3 TrackPosition (XRNode node) {
+		return InputTracking.GetLocalPosition(node);
 	}
 
 }
