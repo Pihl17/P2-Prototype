57845b6f456528a1bc6e5385361093c204186ca3
Zoffer <mzoffm17@student.aau.dk>
2018-04-26 16:22:09 +0200

    Revert "Merge branch 'NewNodeTracking' of https://github.com/Pihl17/P2-Prototype into NewNodeTracking"
    This reverts commit 0a0918b64c659a90d75968a0034da58bbfcd64f3, reversing
    changes made to ef39f4b9bab3554ebb10782eb8d2eaba5ab1db1d.

--- P2 Prototype/Assets/_Scripts/Movement.cs
+++ P2 Prototype/Assets/_Scripts/Movement.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	public NodeScript[] nodes; //The array storing the nodes, they are assigned through the editor.
+	public NodeScript[] nodes;
 	public float angleMargin = 45;
     public float distancemargin = 0.0001f;
 
 
     public float speed = 5;
-    Vector3 moveDirection = Vector3.right; public Vector3 GetMoveDirection() { return moveDirection; } //Used so that the ShowUserDirectionScript can get the direction.
-   
-
-    // Update is called once per frame
-    void Update () {
+	
+	// Update is called once per frame
+	void Update () {
 		foreach (NodeScript node in nodes) {
 			if (CheckRequiredMovement(node.deltaPos)) {
 				Move(node.deltaPos.magnitude);
@@ -22,15 +20,13 @@ public class Movement : MonoBehaviour {
 		}
 	}
 
-    //Makes the user move by adding a force to their object.
 	void Move(float distance) {
-        GetComponent<Rigidbody>().AddForce(distance * speed * moveDirection / Time.deltaTime);
+        GetComponent<Rigidbody>().AddForce(distance * speed * Vector3.right / Time.deltaTime);
     }
 
-    //Checks if the movement of the tracking devices are in the correct direction and distance.
 	bool CheckRequiredMovement(Vector3 direction) {
 		if (direction == Vector3.zero || direction.magnitude < distancemargin) return false; 
-		float angle = Vector3.Angle(new Vector3(0,1,1), direction);
+		float angle = Vector3.Angle(Vector3.forward, direction);
 		return (angle <= angleMargin || angle >= 180-angleMargin);
 
 	}
--- P2 Prototype/Assets/_Scripts/ShowUserDirectionScript.cs
+++ P2 Prototype/Assets/_Scripts/ShowUserDirectionScript.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class ShowUserDirectionScript : MonoBehaviour {
 
-    public float distance;
-
-    //Draws gizmos (Icons for the camera, lines, etc.) that can be viewed in the editor window (and in the scene view, if enabled there)
 	void OnDrawGizmos() {
 		Gizmos.color = new Color(0,255,0);
-        Gizmos.DrawRay(transform.position, distance * GetComponent<Movement>().GetMoveDirection()); //draws the line that the user's moving through.
-		Gizmos.DrawWireMesh (GetComponent<MeshFilter> ().sharedMesh, transform.position, transform.rotation, transform.lossyScale); //Draws the mesh
+		Gizmos.DrawRay (transform.position, 50*Vector3.right);
+		Gizmos.DrawWireMesh (GetComponent<MeshFilter> ().sharedMesh, transform.position, transform.rotation, transform.lossyScale);
 	}
 
 }
