2349105f1214eef49d79325a20b79b7a8fdf9362
Zoffer <mzoffm17@student.aau.dk>
2018-04-26 16:00:38 +0200

    Finished the code for walking.

--- P2 Prototype/Assets/_Scripts/Movement.cs
+++ P2 Prototype/Assets/_Scripts/Movement.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	public NodeScript[] nodes;
+	public NodeScript[] nodes; //The array storing the nodes, they are assigned through the editor.
 	public float angleMargin = 45;
     public float distancemargin = 0.0001f;
 
     public float speed = 5;
-	
-	// Update is called once per frame
-	void Update () {
+    Vector3 moveDirection = Vector3.right; public Vector3 GetMoveDirection() { return moveDirection; } //Used so that the ShowUserDirectionScript can get the direction.
+   
+
+    // Update is called once per frame
+    void Update () {
 		foreach (NodeScript node in nodes) {
 			if (CheckRequiredMovement(node.deltaPos)) {
 				Move(node.deltaPos.magnitude);
@@ -19,13 +21,15 @@ public class Movement : MonoBehaviour {
 		}
 	}
 
+    //Makes the user move by adding a force to their object.
 	void Move(float distance) {
-        GetComponent<Rigidbody>().AddForce(distance * speed * Vector3.right / Time.deltaTime);
+        GetComponent<Rigidbody>().AddForce(distance * speed * moveDirection / Time.deltaTime);
     }
 
+    //Checks if the movement of the tracking devices are in the correct direction and distance.
 	bool CheckRequiredMovement(Vector3 direction) {
 		if (direction == Vector3.zero || direction.magnitude < distancemargin) return false; 
-		float angle = Vector3.Angle(Vector3.forward, direction);
+		float angle = Vector3.Angle(new Vector3(0,1,1), direction);
 		return (angle <= angleMargin || angle >= 180-angleMargin);
 
 	}
--- P2 Prototype/Assets/_Scripts/NodeScript.cs
+++ P2 Prototype/Assets/_Scripts/NodeScript.cs
@@ -5,22 +5,23 @@ using UnityEngine.XR;
 
 public class NodeScript : MonoBehaviour {
 
-	public XRNode nodeType;
+	public XRNode nodeType; //Which device the object will react to, assigned through the editor.
 	public Vector3 deltaPos;
 	Vector3 oldPos;
 
 	// Use this for initialization
 	void Start () {
-        InputTracking.disablePositionalTracking = true;
 		oldPos = TrackPosition(nodeType);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //Calculates the vector it had been moved since last frame.
 		deltaPos = TrackPosition(nodeType) - oldPos;
 		oldPos = TrackPosition(nodeType);
 	}
 
+    //Returns the local position of the specified device.
 	Vector3 TrackPosition (XRNode node) {
 		return InputTracking.GetLocalPosition(node);
 	}
