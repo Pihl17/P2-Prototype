bb002e811f34d0ca6891d5246022fa5f4f472f4e
MPihl <mpihl17@student.aau.dk>
2018-04-27 14:57:37 +0200

    Added audio

--- P2 Prototype/Assets/_Scripts/Movement.cs
+++ P2 Prototype/Assets/_Scripts/Movement.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	public NodeScript[] nodes;
+	public NodeScript[] nodes; //The array for storing the nodes, they are assigned through the editor.
 	public float angleMargin = 45;
     public float distancemargin = 0.0001f;
 
-
     public float speed = 5;
-	
+	Vector3 moveDirection = Vector3.right; public Vector3 GetMoveDirection() {return moveDirection;}
+
 	// Update is called once per frame
 	void Update () {
 		foreach (NodeScript node in nodes) {
@@ -20,13 +20,15 @@ public class Movement : MonoBehaviour {
 		}
 	}
 
+	//Makes the user move by adding a force to their object.
 	void Move(float distance) {
-        GetComponent<Rigidbody>().AddForce(distance * speed * Vector3.right / Time.deltaTime);
+		GetComponent<Rigidbody>().AddForce(distance * speed * moveDirection / Time.deltaTime);
     }
 
+	//Checks if the movement of the tracking devices are in the correct direction and distance.
 	bool CheckRequiredMovement(Vector3 direction) {
 		if (direction == Vector3.zero || direction.magnitude < distancemargin) return false; 
-		float angle = Vector3.Angle(Vector3.forward, direction);
+		float angle = Vector3.Angle(new Vector3(0,1,1), direction);
 		return (angle <= angleMargin || angle >= 180-angleMargin);
 
 	}
--- P2 Prototype/Assets/_Scripts/ShowUserDirectionScript.cs
+++ P2 Prototype/Assets/_Scripts/ShowUserDirectionScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ShowUserDirectionScript : MonoBehaviour {
 
+	public float distance;
+
+	//Draws "Gizmos" (Icons for the camera, lines, etc.) that can be viewed in the editor view (and in the scene view, if enabled there).
 	void OnDrawGizmos() {
 		Gizmos.color = new Color(0,255,0);
-		Gizmos.DrawRay (transform.position, 50*Vector3.right);
-		Gizmos.DrawWireMesh (GetComponent<MeshFilter> ().sharedMesh, transform.position, transform.rotation, transform.lossyScale);
+		Gizmos.DrawRay (transform.position, distance * GetComponent<Movement>().GetMoveDirection()); //Draws the line that the user's moving through.
+		Gizmos.DrawWireMesh (GetComponent<MeshFilter> ().sharedMesh, transform.position, transform.rotation, transform.lossyScale); //Draws the mesh.
 	}
 
 }
