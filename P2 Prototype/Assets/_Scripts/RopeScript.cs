37909854c009d95621d4e6c6f69af5ec1c45064d
Zoffer <mzoffm17@student.aau.dk>
2018-04-30 09:19:47 +0200

    mZ- Update
    mZ Update30-04

--- P2 Prototype/Assets/_Scripts/RopeScript.cs
+++ P2 Prototype/Assets/_Scripts/RopeScript.cs
@@ -4,23 +4,53 @@ using UnityEngine;
 
 public class RopeScript : MonoBehaviour {
 
-	public GameObject dummi;
-	GameObject PlankOne;
-	//GameObejct LastPlank;
+	public GameObject [] planks =  new GameObject[1];
+    public float newZ;
+    public GameObject rope;
+    public float newX = 1.1f;
+    int rotateX = 90;
+    private float newY = -0.9f;
+    private float newYY = -0.1f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Vector3 ropePos = new Vector3();
+    Vector3 reduce = new Vector3();
+    Quaternion rot;
+    Vector3 rot2 = new Vector3();
+    //Vector3[] plank [2];
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+
+        
+      //  -newX,-newY 
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        PlaceRope();
+    }
 
-	void onBrigde(){
-		//if (dummi.translate.localPosition.x>)
-	}
+    void PlaceRope()
+    {
+        //Quaternion.Euler
+     //  rot = Quaternion.Euler(planks[0].transform.rotation.x/4, planks[0].transform.rotation.y, planks[0].transform.rotation.z);
+        // rot2 = rot.z*;
+       // rot = Quaternion.Euler(planks[0].transform.localRotation.x,planks[0].transform.localRotation.y,planks[0].transform.localRotation.z);
+            //= new Vector3(planks[0].transform.rotation.x, planks[0].transform.rotation.y, planks[0].transform.rotation.z);
+            //   reduce = new Vector3(planks[1].transform.localPosition.x, planks[1].transform.localPosition.y, planks[1].transform.localPosition.z);
+            // ropePos = new Vector3(planks[0].transform.localPosition.x + planks[1].transform.localPosition.x / 2, planks[0].transform.localPosition.y + planks[1].transform.localPosition.y / 2, planks[0].transform.localPosition.z + planks[1].transform.localPosition.z / 2);
+        ropePos = new Vector3(planks[0].transform.position.x-newX, planks[0].transform.position.y+newY, planks[0].transform.position.z+newZ    );
+       // print(reduce);
+         rope.transform.position = ropePos;
+      //  rope.transform.rotation = rot;
+    }
 
 
 }
+
